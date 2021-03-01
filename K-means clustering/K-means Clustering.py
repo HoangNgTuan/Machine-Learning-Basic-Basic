253ed1ec18bc0319be94b51c1b7ffb69d84415ca
WindowsXP_Crash.exe <78265898+HoangNgTuan@users.noreply.github.com>
2021-03-01 18:59:47 +0700

    Create K-means Clustering.py

---
+++ K-means clustering/K-means Clustering.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist  #Calculate distance from 2 data
+np.random.seed(11)
