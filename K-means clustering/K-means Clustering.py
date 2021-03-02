e12d789e72efbcc5b499ff69257614da3fd49e60
WindowsXP_Crash.exe <78265898+HoangNgTuan@users.noreply.github.com>
2021-03-02 10:02:33 +0700

    Update K-means Clustering.py

--- K-means clustering/K-means Clustering.py
+++ K-means clustering/K-means Clustering.py
@@ -1,17 +1,18 @@
 from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial.distance import cdist  #Calculate distance from 2 data
+from scipy.spatial.distance import cdist  # Calculate distance from 2 data
 np.random.seed(11)
 
-means = [[2, 2], [8, 3], [3, 6]]
-conv = [[1, 0], [0, 1]]
-N = 500
-X0 = np.random.multivariate_normal(means[0], cov, N)
-X1 = np.random.multivariate_normal(means[1], cov, N)
-X2 = np.random.multivariate_normal(means[2], cov, N)
+means = [[2, 2], [9, 2], [4, 9]] # Manually gennerate coordinate of means
+conv = [[2, 0], [0, 2]]
+n_sample = 500
+n_cluster = 3 # K = 3 (We decide)
+X0 = np.random.multivariate_normal(means[0], cov, n_sample)
+X1 = np.random.multivariate_normal(means[1], cov, n_sample)
+X2 = np.random.multivariate_normal(means[2], cov, n_sample)
 
 X = np.concatenate((X0, X1, X2), axis = 0)
-K = 3
+
 
 original_label = np.asarray([0]*N + [1]*N + [2]*N).T
